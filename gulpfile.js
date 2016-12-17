88a022ba78813efc06afc2ced1f163e882bc70d6
h404bi <h404bi@gmail.com>
2016-12-17 21:16:53 +0800

    v0.5.0

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var gulp = require('gulp');
+var del = require('del');
+var flatten = require('gulp-flatten');
+var rename = require('gulp-rename');
+var stylus = require('gulp-stylus');
+var postcss = require('gulp-postcss');
+var postcssImport = require('postcss-import');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+var target = 'source/assets/';
+
+gulp.task('clean', function () {
+  return del([target]);
+});
+
+gulp.task('copy:image', ['clean'], function () {
+  return gulp.src('source/_images/*')
+    .pipe(gulp.dest(target + 'images'));
+});
+
+gulp.task('copy:fonts', ['copy:image'], function () {
+  return gulp.src('source/_fonts/**/*')
+    .pipe(gulp.dest(target + 'fonts'));
+});
+
+gulp.task('build:js', ['copy:fonts'], function () {
+  return gulp.src('source/_js/*.js')
+    .pipe(gulp.dest(target + 'js'));
+});
+
+gulp.task('build:css', ['build:js'], function () {
+  var processors = [
+    postcssImport,
+    autoprefixer({browsers: ['> 2% in CN', 'last 2 version']}),
+    cssnano()
+  ];
+  return gulp.src('source/_css/style.styl')
+    .pipe(stylus())
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(target + 'css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['source/_**/**/*'], ['build:css']);
+});
+
+gulp.task('default', ['build:css', 'watch']);
